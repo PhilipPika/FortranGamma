098cd1d6756faf899c8ed03d229e779ea1753b37
Philip <philip.pika@bristol.ac.uk>
2017-06-25 17:33:03 +0200

    Completed Translation from Matlab to Fortran. It's alivegs

--- MultiG.f
+++ MultiG.f
@@ -2,94 +2,81 @@ c
 c      SUBROUTINE MULTIG
 c
       subroutine MultiG()
-!        include 'common_geo.inc'
-!        include 'common.inc'
-        real*8 kvalue(14,1), fvalue(14,1), POC(1,14), SUMPOC(1,1),POC0,z
-     +,a,b,Aa,Bb,w0,db0
+c        include 'common_geo.inc'
+c        include 'common.inc'
+      real*8 kvalue(14,1),fvalue(14,1), POC(1000,14), SUMPOC(1000,1),
+     +POC0,a,b,Aa,Bb,w0,db0,z,zz,SPOC(1000,1)
         integer nG,xx,i
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-      open (unit = 1, file = 'f_POC_Values.dat')
-              read(1,*) (fvalue(i,1), i = 1,14)
+c_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+c_/                        Loading in data                           _/        
+c_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+      write(*,*) 'This is MultiG being called!'
+      open (unit=1, file='f_POC_Values.dat')
+              read(1,*) (fvalue(i,1), i=1,14)
               write(*,*) "Loading f: ",i ," done"
       close(1)
-
-      open (unit = 1, file = 'k_POC_Values.dat')
-              read(1,*) (kvalue(i,1), i = 1,14)
+      open (unit=1, file='k_POC_Values.dat')
+              read(1,*) (kvalue(i,1), i=1,14)
               write(*,*) "Loading k: ",i ," done"
       close(1)
+c_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+c_/                        Loading in parameter                      _/        
+c_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
-      write(*,*) 'This is MultiG being called!'
-!     if(xx_pos.gt.xxbiot)then
-!     elseif(xx_poxx.le.xxboit)
- 
-      POC0=13.4
+      POC0=13.3
       w0=0.11
       db0=27
+
+c_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+c_/                        Start depth for-loop                      _/        
+c_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
       
-      do xx = 1,1
-      z = (xx-1)/100
-     
+      do xx=1,1000
+      zz=(xx-1)
+      z=zz/100
+c_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+c_/                        Start fractions for-loop                  _/        
+c_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
       SUMPOC=0.0
-      do nG = 1,14
+      do nG=1,14
 
-        a = (w0-(w0**2+4*db0*kvalue(nG,1))**(1/2))/(2*db0);
-c        write(*,*) 'a',a
-        b = (w0+(w0**2+4*db0*kvalue(nG,1))**(1/2))/(2*db0);
-c        write(*,*) 'b',b
-        !b.c. specific
-        Aa = -(fvalue(nG,1)*POC0*exp(b*z)*b)/(exp(a*z)*a-exp(b*z)*b);
-c        write(*,*) 'Aa',Aa
-        Bb = (exp(a*z)*POC0*a*fvalue(nG,1))/(exp(a*z)*a-exp(b*z)*b);
-c        write(*,*) 'Bb',Bb
+!        a=(w0-(w0**2+4*db0*kvalue(nG,1))**(0.5))/(2*db0)!Works OK
+!        a=(w0-(w0**2+4*db0*kvalue(nG,1))**(1/2))/(2*db0)!Doesn't work
+        a=(w0-sqrt(w0**2+4*db0*kvalue(nG,1)))/(2*db0)
+!        b=(w0+(w0**2+4*db0*kvalue(nG,1))**(0.5))/(2*db0)!Works OK
+!        b=(w0+(w0**2+4*db0*kvalue(nG,1))**(1/2))/(2*db0)!Doesn't work
+        b=(w0+sqrt(w0**2+4*db0*kvalue(nG,1)))/(2*db0)
+C Specific to boundary conditions        
+        Aa=-(fvalue(nG,1)*POC0*exp(b*z)*b)/(exp(a*z)*a-exp(b*z)*b)
+        Bb=(exp(a*z)*POC0*a*fvalue(nG,1))/(exp(a*z)*a-exp(b*z)*b)
 
-        POC(xx,nG)=Aa*exp(a*z) + Bb*exp(b*z);
+        POC(xx,nG)=Aa*exp(a*z)+Bb*exp(b*z);
 
-       ! SUMPOC(xx,1) = SUMPOC(xx-1,1) + (POC(xx, i = 1,14))
-       ! SUMPOC(xx,1) = SUMPOC(xx-1,1) + POC(xx,nG)
-        SUMPOC(xx,1) = SUMPOC(xx,1) + POC(xx,nG)
+C       Debugging part
+!        write(*,*) 'z',z,'nG',nG,'a',a,'b',b,'Aa',Aa,'Bb',Bb,'POC',POC(x
+!     +x,nG)
+!      write(*,*) 'z',z,'nG',nG, 'POC', POC(xx,nG),'SPOC',SUMPOC(xx,1)
+!      write(*,*) 'k', nG,'=',kvalue(nG,1),'F',nG,'=',fvalue(nG,1)
+      
+
+       SUMPOC(xx,1)=SUMPOC(xx,1)+POC(xx,nG)! Works now as as those below
+c       work too
       enddo
-!        SUMPOC(xx,1) = SUMPOC(xx-1,1) + POC(xx,nG)
+      write(33,*) SUMPOC(xx,1)
+      
+!      SUMPOC(xx,1)=POC(xx,1)+POC(xx,2)+POC(xx,3)+POC(xx,4)+POC(xx,5)+POC
+!     +(xx,6)+POC(xx,7)+POC(xx,8)+POC(xx,9)+POC(xx,10)+POC(xx,11)+POC(
+!     +xx,12)+POC(xx,13)+POC(xx,14)
+!      write(33,*) SUMPOC(xx,1)
+!      SPOC=0.0
+!      DO i=1,14
+!      SPOC(xx,1)=SPOC(xx,1)+POC(xx,i)
+!      ENDDO
+!      write(44,*) SPOC(xx,1)
+     
       enddo
+      
       write(22,*) POC
-      write(33,*) SUMPOC
-      END
 
-!      a11=(w0-(w0**2+4*db0(j)*kk)**(1/2))/(2*db0(j));
-!      a11_(i)=a11;
-!      b11=(w0+(w0**2+4*db0(j)*kk)**(1/2))/(2*db0(j));
-!      b11_(i)=b11;
-!      
-!      a21=(-kk/w0);
-!      a21_(i)=a21;
-!      
-!      A11=-(FF*POCt*b11*exp(b11*zbio))/(a11*exp(a11*zbio)-b11*exp(b11*zbio)); % The same as A above!!
-!      A11_(pp,i) = A11;
-!      
-!      valid = exp(a21*zbio);
-!      if valid <= 10**-21
-!          valid = 10**-20;
-!      end
-!      
-!      A21=(A11*(exp(a11*zbio)-exp(b11*zbio))+FF*POCt*exp(b11*zbio))/valid;
-!      
-!      %         A21_(pp,i)=A21;
-!      %
-!      %         if isnan(A21) == 1 || A21 == Inf || A21 == -Inf
-!      %             A21 = 1e-20;
-!      %         elseif  A21 >= 0.5 || A21 <= -0.5
-!      %             A21 = 1e-20;
-!      %         end
-!      %         A21__(pp,i)=A21;
-!      
-!      if(xx(pp)<zbio)
-!          POC(pp,i)=A11*(exp(a11*xx(pp))-exp(b11*xx(pp)))+FF*POCt*exp(b11*xx(pp));
-!      else
-!          
-!          %             valevale=exp(a21*xx(pp));
-!          %             if valevale == 0
-!          %                 valevale = 1e-20;
-!          %             end
-!          
-!          POC(pp,i)=A21*exp(a21*xx(pp));
-!      END
+      END
